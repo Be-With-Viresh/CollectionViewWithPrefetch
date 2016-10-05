9fe7a1885c0d97c7b5c9819ed8eaa60493eec56f
Viresh Madabhavi <viresh@vireshs-mac-mini.local>
2016-10-05 16:02:11 +0530

    Initial Commit

---
+++ ColllectioViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColllectioViewDemo
+//
+//  Created by Viresh Madabhavi on 05/10/16.
+//  Copyright © 2016 Viresh Madabhavi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
