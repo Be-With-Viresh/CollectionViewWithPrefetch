c2d363e8d9c05fac12fcd776584bc8429216017f
Viresh Madabhavi <viresh@Vireshs-Mac-mini.local>
2016-10-06 11:14:09 +0530

    FC

---
+++ ColllectioViewDemo/CollectionCellCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionCellCollectionViewCell.swift
+//  ColllectioViewDemo
+//
+//  Created by Viresh Madabhavi on 05/10/16.
+//  Copyright © 2016 Viresh Madabhavi. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCellCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
